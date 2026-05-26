1e793c61ccc245d57de2577fdfed26ce9048d666
Fs1ooz <spinaf64@gmail.com>
2026-05-26 21:13:47 +0200

    Aggiornamento

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"b683af0cecc0f51dac1968c19f691aa3111b58
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2826370334"
+    serviceWorkerVersion: "357228622"
   }
 });
